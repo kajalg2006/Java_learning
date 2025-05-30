550996a4ad233d5e836f8c1e3b9af3b6d02af6c0
Kajal Gupta <kaju27gupta@gmail.com>
2025-05-30 21:41:35 +0530

    changes here

--- 32_Exception_Handling/K.java
+++ 32_Exception_Handling/K.java
@@ -1,3 +1,12 @@
-public class K {
-    
+class K extends RuntimeException {
+    public static void main(String[] args) throws Throwable {
+        Error x = new Error();
+        // Exception x = new Exception();
+        // Throwable x = new Throwable();
+        // ArithmeticException x = new ArithmeticException();
+        // K x = new K();
+        // Object x = new Object();
+
+        throw x;
+    }    
 }
