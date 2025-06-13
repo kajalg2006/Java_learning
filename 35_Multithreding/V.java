cf7e1316fc81e8c8004aa039ff63ff7b1c2c6045
Kajal Gupta <kaju27gupta@gmail.com>
2025-06-13 23:05:35 +0530

    some more question on multithreading

---
+++ 35_Multithreding/V.java
@@ -0,0 +1,35 @@
+class Y1Runnable implements Runnable {
+    public void run() {
+
+    }
+}
+
+class Y2Runnable implements Runnable {
+    public void run() {
+
+    }
+}
+
+class V {
+    static V aa = new V();
+    static V bb = new V();
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Y1Runnable(), "A");
+        Thread t2 = new Thread(new Y2Runnable(), "B");
+
+        t1.start();
+        t2.start();
+    }
+
+    synchronized void pro(int i) {
+        Thread x = Thread.currentThread();
+        System.out.println(i + " - " + x.getName());
+    }
+
+    synchronized void info(int i) {
+        Thread x = Thread.currentThread();
+        System.out.println(i + " - " + x.getName());
+    }
+
+}
