2271fc24a8524d13d9379c2649ad12188ef46ac1
Kajal Gupta <kaju27gupta@gmail.com>
2025-06-14 23:30:36 +0530

    Some more code on multithreading

--- 35_Multithreding/V.java
+++ 35_Multithreding/V.java
@@ -1,12 +1,16 @@
 class Y1Runnable implements Runnable {
     public void run() {
-
+       for(int i=0;i<30;i++) {
+            V.aa.pro(i);
+        }
     }
 }
 
 class Y2Runnable implements Runnable {
     public void run() {
-
+       for(int i=0;i<30;i++) {
+            V.bb.info(i);
+        }
     }
 }
 
