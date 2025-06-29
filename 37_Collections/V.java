66354ac2a11f5f577724efbd6733cbe5d4c8f4bd
Kajal Gupta <kaju27gupta@gmail.com>
2025-06-29 22:27:42 +0530

    ArrayList

--- 37_Collections/V.java
+++ 37_Collections/V.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+
 public class V {
-    
+    public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<Student>();
+
+        Student a = new Student("jay", 23);
+        Student b = new Student("raghav", 18);
+        Student c = new Student("golu", 9);
+        Student d = new Student("vikramaditya", 12);
+        Student e = new Student("om", 17);
+
+        list.add(a);
+        list.add(b);
+        list.add(c);
+        list.add(d);
+        list.add(e);
+
+        System.out.println(list);
+
+    }
 }
